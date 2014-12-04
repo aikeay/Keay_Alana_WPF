02183063e859dee63d7fcb4d36ba615e458309bf
aikeay <aikeay@fullsail.edu>
2014-12-04 11:19:41 -0800

    added personal information
    added personal information

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -6,4 +6,11 @@
 
 //alert("test test");
 
-// create a calculator that 
+// create a calculator which is how many hours a month you spend on the computer.
+
+// ask user for their name
+var name = prompt("What is your name?");
+
+alert("Hello "+name+". Now let's figure out how many hours on average do you spend on the computer a month.");
+
+
