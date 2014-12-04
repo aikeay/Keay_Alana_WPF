78fddba1c468c7c4f7500004cc147b5114b7651d
aikeay <aikeay@fullsail.edu>
2014-12-03 21:42:59 -0800

    second variable added
    second variable added

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -12,5 +12,11 @@
 // ask the user their name
 var name = prompt("Please type in your name?");
 
+//use variable and inform user what we are going to figure out
 alert("welcome "+name+"! + Let's figure how many gallons of milk you go through in a year.");
+
+// ask how many gallons of milk you go through in a month
+var month = prompt("How many gallons of milk do you drink in a month?");
+
+// use the variable month and multiple it by 12
 
