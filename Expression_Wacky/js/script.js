85b0dff710e5f0ca4dfdc66e0b904f8997ab173c
aikeay <aikeay@fullsail.edu>
2014-12-04 14:01:13 -0800

    Get the users name
    Ask the user for their name

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -5,4 +5,12 @@
 * 12-4-2014
  */
 
-alert("making sure that js is connected!!");
+//alert("making sure that js is connected!!");
+
+// create a calculator that can calculate the number of monsters you could fix in your room
+
+// ask user for their name
+var name = prompt("What is your name?");
+
+alert("Hello "+name+"! Where are going to find out how many monsters that you could fit in your room but first we are going to be some information.");
+
