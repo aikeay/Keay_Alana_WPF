aa2f95daca473ac6bd8b64d86855e6f12a885630
aikeay <aikeay@fullsail.edu>
2014-12-03 21:28:35 -0800

    Asked for users name
    Asked user for their name

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -5,5 +5,12 @@
  * 12-3-2014
  */
 
+//alert("test test.");
 
-alert("test test.");
+// create personal calculator which is how many gallons of milk do you go through in a year
+
+// ask the user their name
+var name = prompt("Please type in your name?");
+
+alert("welcome "+name+"! + Let's figure how many gallons of milk you go through in a year.");
+
