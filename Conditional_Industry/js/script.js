dbbcab0c08176be584ad0976e67af4d8a8f7323b
aikeay <aikeay@fullsail.edu>
2014-12-11 15:51:24 -0800

    Final conditional Industry Assignment
    Final conditional Industry Assignment

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -11,6 +11,7 @@
 
 var career = prompt("How much do you make a month creating websites?");
 console.log(career);
+
 var monthly = prompt("How much is your cost of living?")
 console.log(monthly);
 
@@ -18,6 +19,7 @@ console.log(monthly);
 var roommate = prompt("How much does your roommates to help you cover your bills if no roommates enter 0?")
 console.log(roommate);
 
-
 // does you career bring in enough more to cover your monthly bills
 (roommate+career > monthly)? console.log("You make enough to cover your cost of living!") : console.log("You need to get another job or kick up it on your creation of websites.");
+
+//creating webites is part of my industry
