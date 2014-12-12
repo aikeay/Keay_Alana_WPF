cb45bff329aaf4c079d13f4205ce7a264d1a1995
aikeay <aikeay@fullsail.edu>
2014-12-11 17:33:56 -0800

    added 3 variable
    added 3 variable

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -8,9 +8,15 @@
 
 // create conditional wacky assignment. how many birds does it take to make your house fly
 
-var house = prompt("How much does your house  in lbs?")
+var house = prompt("How much does your house in lbs?")
 console.log(house)
 
 var birds = prompt("how many birds do you have?")
 console.log(birds)
 
+var friendsBirds = prompts("Does your friends have any birds you can use? If none enter 0.")
+
+// for each lb you need 2 birds
+var possibleFlight = house*2
+
+if(possibleFlight<birds )
