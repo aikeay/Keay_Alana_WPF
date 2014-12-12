60698c1725442d0ad27135b68a55c59bd2352077
aikeay <aikeay@fullsail.edu>
2014-12-11 17:08:11 -0800

    added the first variable
    added the first variable
    house

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -4,4 +4,10 @@
  * Conditional Industry Assignment
  * created on 12-11-2014
  */
-alert("test test")
+//alert("test test")
+
+// create conditional wacky assignment. how many birds does it take to make your house fly
+
+var house =prompt("How much does your house weight?")
+
+// if 
