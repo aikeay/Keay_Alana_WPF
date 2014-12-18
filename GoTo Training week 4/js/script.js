60c9f82495321b6d8bf1592b0d0c4cd8dc61f26c
aikeay <aikeay@fullsail.edu>
2014-12-18 10:15:40 -0800

    starting GoTo Training wk4

---
+++ GoTo Training week 4/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alanakeay on 12/18/14.
+ */
+
+//alert ("Test")
+
+
+
+// ask the user for a min number
+var min = prompt("Let's get a random number between two values.\n {please type in the min valve:");
+
