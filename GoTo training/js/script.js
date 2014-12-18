5d52127b8ffd7fc3be5c0b567fa46c383dd9892b
aikeay <aikeay@fullsail.edu>
2014-12-18 08:37:22 -0800

    Length property of array

--- GoTo training/js/script.js
+++ GoTo training/js/script.js
@@ -4,4 +4,20 @@
 
 //alert ("testing testing");
 
-FSVFDVD
+var bowlOfFruit = ["apple","banana","pear","peach"];
+
+// console.log the whole array
+console.log(bowlOfFruit);
+
+// how can i get one fruit inside the bowl
+console.log(bowlOfFruit[1]);
+
+// how many fruits are in the bowl?
+// how many iteams are in the array?
+// Length of the array
+//Dot Syntax -- "use a period!"
+console.log(bowlOfFruit.length);
+
+//What is the last index number of the array using the length property
+//Last index is Always one less than the length
+// bowlOfFruit.Legth - 1 --- is the last index #
