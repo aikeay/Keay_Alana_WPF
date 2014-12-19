10f95d534b6325734405c4fd6a6e1cdc4061118e
aikeay <aikeay@fullsail.edu>
2014-12-18 20:35:23 -0800

    started function

--- Function_Wacky/js/script.js
+++ Function_Wacky/js/script.js
@@ -8,19 +8,21 @@
 //alert("Test")
 
 // create a calculator that you can use to figure out how many dogs you will need as transport
-//create variable for distance
-var distance = prompt("How many miles do you want to travel?")
-while(distance==="" || isNaN(distance)){
-    // reprompt the user
-    distance = prompt(" Please do not leave blank and only use numbers. \n Please type a distance:")
-}
-    console.log(distance)
+
 //create weight variable
 var weight = prompt("Please type in your weight in lbs")
 while(weight==="" || isNaN(weight)){
     // reprompt the user
     weight = prompt(" Please do not leave blank and only use numbers. \n Please type a weight:")
 }
-//create the variable for how many lbs dogs can pull
-var dogsPull = 2
-function 
+    console.log(weight)
+//create the variable for how many lbs dogs can pull because dogs never get tired
+var dogsPull = Number(2)
+
+var dogsNeed = function(weight, dogsPull ){
+  var dogs = Math.round(weight/dogsPull)
+    return dogs
+}
+
+var transport = dogsNeed()
+console.log(transport)
