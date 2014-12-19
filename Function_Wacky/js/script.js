d2f389dd993eb55be669b3bb40079dd27f1efc15
aikeay <aikeay@fullsail.edu>
2014-12-18 20:16:52 -0800

    create weight variable

--- Function_Wacky/js/script.js
+++ Function_Wacky/js/script.js
@@ -15,3 +15,9 @@ while(distance==="" || isNaN(distance)){
     distance = prompt(" Please do not leave blank and only use numbers. \n Please type a distance:")
 }
     console.log(distance)
+//create weight variable
+var weight = prompt("Please type in your weight in lbs")
+while(weight==="" || isNaN(weight)){
+    // reprompt the user
+    weight = prompt(" Please do not leave blank and only use numbers. \n Please type a weight:")
+}
