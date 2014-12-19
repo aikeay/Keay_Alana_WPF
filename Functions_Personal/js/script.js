e5d6eb5b3b0d193e99d22b4eea39f01f698ea9e8
aikeay <aikeay@fullsail.edu>
2014-12-18 17:03:18 -0800

    validated the 1st variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -11,3 +11,12 @@
 // create variables to collect data for windows
 
 var windows = prompt("How many windows would you like to replace on your house? Please type in a number:");
+// make sure you validate
+while(windows==="" || isNaN(windows)) {
+    // Reprompt user
+    if (windows === "") {
+        min = prompt("Please do not leave blank \n Please type in a amount")
+    } else {
+        min = prompt("please type in a min number \n Please type in a number:");
+    }
+}
