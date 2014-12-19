fdc53d70d7c01d0bcfb677c1fffdf4552dde4ae9
aikeay <aikeay@fullsail.edu>
2014-12-18 16:59:51 -0800

    created 1st variable
    created window variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,4 +5,9 @@
  * Created on 12-17-2014
  */
 
-alert("test test")
+//alert("test test")
+
+// calculate the cost of replacing windows and doors on a house.
+// create variables to collect data for windows
+
+var windows = prompt("How many windows would you like to replace on your house? Please type in a number:");
